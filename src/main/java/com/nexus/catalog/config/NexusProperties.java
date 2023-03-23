823353ce75f06cea7f537b279a9b084ec0e51ebd
Lekoba Kutu <ekutu@mweb.co.za>
2023-03-23 10:16:12 +0200

    Adding and Validation Kubernetes manifests to the workflow

--- src/main/java/com/nexus/catalog/config/NexusProperties.java
+++ src/main/java/com/nexus/catalog/config/NexusProperties.java
@@ -1,7 +1,9 @@
 package com.nexus.catalog.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
+@Component
 @ConfigurationProperties(prefix = "nexus")
 public class NexusProperties {
 
